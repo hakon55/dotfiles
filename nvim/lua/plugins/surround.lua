d401379c546b916acda7ab8f6e00703ee5bd4815
Håkon Størdal <30749741+hakon55@users.noreply.github.com>
2023-10-20 16:20:55 +0200

    Another one

---
+++ nvim/lua/plugins/surround.lua
@@ -0,0 +1,16 @@
+return {
+  "echasnovski/mini.surround",
+  vscode = true,
+  opts = {
+    vscode = true,
+    mappings = {
+      add = "sa",
+      delete = "sd",
+      replace = "sr",
+      find = "sf",
+      find_left = "sF",
+      highlight = "sh",
+      update_n_lines = "sn",
+    },
+  },
+}

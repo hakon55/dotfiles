d65443e9b9c5e8bd0ad0f60bf22efb474c0f0024
Håkon Størdal <30749741+hakon55@users.noreply.github.com>
2023-10-20 16:22:22 +0200

    Fishy

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+abbr --add gco git checkout
+abbr --add dc docker compose
+abbr --add fishconfig nvim ~/.config/fish/config.fish
+abbr --add nvimconfig nvim ~/.config/nvim/init.lua
+abbr --add remove-swap-neovim rm -rf ~/.local/state/nvim/swap/
+
+abbr --add config ~/.config/
+abbr --add applications ~/.local/state/applications
+abbr --add drivers "lspci -k | grep -A 2 -E \"(VGA|3D)\""
+
+# NeoVide env-variables
+# set -x NEOVIDE_MULTIGRID true
+# set -x NEOVIDE_MAXIMIZED true
+# set -x NEOVIDE_FRAME none
+
+nvm use latest -s
+starship init fish | source

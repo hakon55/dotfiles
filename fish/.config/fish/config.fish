cf1f5ec079961295210ecd4bc1fafd9500b8f398
Håkon Størdal <30749741+hakon55@users.noreply.github.com>
2024-01-28 16:50:34 +0100

    First commit

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,22 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+abbr --add dc docker compose
+abbr --add fishconfig helix ~/.config/fish/config.fish
+abbr --add nvimconfig nvim ~/.config/nvim/init.lua
+
+abbr --add yay paru
+
+abbr --add config ~/.config/
+abbr --add applications ~/.local/state/applications
+abbr --add drivers "lspci -k | grep -A 2 -E \"(VGA|3D)\""
+abbr --add dotfiles chezmoi
+abbr --add hx helix
+
+# NeoVide env-variables
+# set -x NEOVIDE_MULTIGRID true
+# set -x NEOVIDE_MAXIMIZED true
+# set -x NEOVIDE_FRAME none
+
+starship init fish | source

21082ec1107ddccc43f3a1d9cd3215076217754c
Håkon Størdal <30749741+hakon55@users.noreply.github.com>
2024-12-03 22:29:28 +0000

    litt conf til fisken

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,8 +3,13 @@ set -g fish_greeting
 # fish_add_path /home/hakon/.local/bin
 
 abbr dc docker compose
+
 abbr g git
+abbr ga git add 
 abbr gs git status
+abbr gp git push
+abbr gl git pull
+abbr gc git commit
 
 set -x EDITOR hx
 set -g fish_key_bindings fish_hybrid_key_bindings

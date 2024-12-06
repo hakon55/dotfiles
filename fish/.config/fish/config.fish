d1ff0b2d5fac7834f9832157d6cb0481c41d06b1
Håkon Størdal <Hakon.Stordal@skatteetaten.no>
2024-12-06 15:13:04 +0100

    Flere endringer

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,9 @@
 set -g fish_greeting
 
 # fish_add_path /home/hakon/.local/bin
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/opt/node@20/bin
+fish_add_path /Users/m01345/scripts
 
 abbr dc docker compose
 
@@ -9,9 +12,10 @@ abbr ga git add
 abbr gs git status
 abbr gp git push
 abbr gl git pull
+abbr gf git fetch
 abbr gc git commit
+abbr gco git checkout
 
-set -x EDITOR hx
 set -g fish_key_bindings fish_hybrid_key_bindings
 
 
